ca1d8f8eee209f443885abfe06884df71ff5c28c
Sonu9142 <109494658+Sonu9142@users.noreply.github.com>
2023-12-14 21:18:49 +0530

    all setup done

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connectDB= async ()=>{
         const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MONGODB CONNECTED !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("mongodb connection failed",error);
+        console.log("mongodb connection failed......",error);
         process.exit(1);
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path:'./env'});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 
 dotenv.config({
@@ -14,7 +15,7 @@ connectDB()
         })
     })
     .catch((err)=>{
-        console.log("MONGO db connection failed",err)
+        console.log("MONGO db connection failed...",err)
     })
 
 
